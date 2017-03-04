fa1f0a8d049562c7d011a26d4e7f1acb90536091
Paul Newland <paulnewland@Pauls-MacBook-Air.local>
2017-03-03 19:48:48 -0800

    Integrated new and create actions for portfolio items

--- app/controllers/portfolio_items_controller.rb
+++ app/controllers/portfolio_items_controller.rb
@@ -2,4 +2,26 @@ class PortfolioItemsController < ApplicationController
 	def index
 		@portfolio_items = PortfolioItem.all
 	end
+
+	def new
+		@portfolio_item = PortfolioItem.new
+	end
+
+	def create
+		@portfolio_item = PortfolioItem.new(portfolio_item_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolio_items_path, notice: 'Portfolio item was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+	end
+
+private
+	
+	def portfolio_item_params
+      params.require(:portfolio_item).permit(:title, :subtitle, :body)
+    end
 end
