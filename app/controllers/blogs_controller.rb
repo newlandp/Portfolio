5deba184dc822350a51f686cc92af461b741c462
Paul Newland <paulnewland@Pauls-MacBook-Air.local>
2017-05-25 21:00:14 -0700

    whitelisted  parameter for devise sign-up and edit account

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -65,14 +65,14 @@ class BlogsController < ApplicationController
     redirect_to blogs_url, notice: 'Post status has been updated.'
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.friendly.find(params[:id])
-    end
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_blog
+    @blog = Blog.friendly.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def blog_params
-      params.require(:blog).permit(:title, :body)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def blog_params
+    params.require(:blog).permit(:title, :body)
+  end
 end
